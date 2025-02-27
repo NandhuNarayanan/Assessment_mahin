f514fc94b59c58f99ec66a1e88da2d3f627e902f
NandhuDigimeta <nandhu@digimeta.co.uk>
2025-02-28 05:10:29 +0530

    scroll added

--- src/app/components/client_expectations/client_Expectations.js
+++ src/app/components/client_expectations/client_Expectations.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import styles from "./Client_Expectations.module.css";
 
 export default function () {
   return (
-    <div></div>
-  )
+    <div className={styles.client_ExpectationsContainer}>
+      <div>
+        <h3 className={styles.client_ExpectationsTitle}>
+          Some works that exceed our client’s expectations!
+        </h3>
+      </div>
+      <div className={styles.client_ExpectationsImagearea}>
+        <img
+          src="/Frame1.png"
+          alt="Vercel Logo"
+          className={styles.client_ExpectationsImage}
+        />
+      </div>
+      <div className={styles.client_ExpectationsImagearea}>
+        <img
+          src="/Frame1.png"
+          alt="Vercel Logo"
+          className={styles.client_ExpectationsImage}
+        />
+      </div>
+    </div>
+  );
 }
