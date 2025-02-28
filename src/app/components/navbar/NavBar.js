30aa39d8ff3ac75656bf5ad33e24b2b99897008e
NandhuDigimeta <nandhu@digimeta.co.uk>
2025-02-28 15:23:09 +0530

    toggle button added

--- src/app/components/navbar/NavBar.js
+++ src/app/components/navbar/NavBar.js
@@ -1,27 +1,44 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./NavBar.module.css";
+import Toggle from "../toggle/toggle";
+import { useRouter } from "next/navigation";
 
 export default function NavBar() {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
   return (
     <div className={styles.NavBarmenu}>
       <div>
-        <div>
+        <div onClick={() => router.push("/")}>
           <Image src="/logo.png" alt="Vercel Logo" width={150} height={130} />
         </div>
       </div>
       <div className={styles.menuList}>
         <ul className={styles.menuListName}>
-          <li>Home</li>
+          <li onClick={() => router.push("/")}>Home</li>
           <li>UI/UX</li>
           <li>Mobile App Development</li>
-          <li>Web DevelopmentWeb Development</li>
-          <li>Case Study</li>
+          <li onClick={() => router.push("/desktop3")}>
+            Web DevelopmentWeb Development
+          </li>
+          <li onClick={() => router.push("/desktop2")}>Case Study</li>
         </ul>
       </div>
-      <div>
+      <div className={styles.menuicon} id="menu">
+        <button className={styles.menuOpen} onClick={() => setOpen(!open)}>
+          <Image
+            src="/hamburger.png"
+            alt="Vercel Logo"
+            width={50}
+            height={50}
+          />
+        </button>
         <button className={styles.menuButton}>Contact</button>
       </div>
+      {open && <Toggle setOpen={setOpen} />}
     </div>
   );
 }
---
+++ src/app/components/toggle/toggle.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Toggle.module.css";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+export default function toggle({ setOpen }) {
+      const router = useRouter()
+  return (
+    <div className={styles.menuContainer}>
+      <button className={styles.menuClose} onClick={() => setOpen(!open)}>
+        <Image src="/delete.png" alt="Vercel Logo" width={50} height={50} />
+      </button>
+
+      <div className={styles.menuList}>
+        <ul className={styles.menuListName}>
+          <li onClick={() => router.push("/")}>Home</li>
+          <li>UI/UX</li>
+          <li>Mobile App Development</li>
+          <li onClick={() => router.push("/desktop3")}>Web DevelopmentWeb Development</li>
+          <li onClick={() => router.push("/desktop2")}>Case Study</li>
+          <button className={styles.menuButton}>Contact</button>
+        </ul>
+      </div>
+    </div>
+  );
+}
